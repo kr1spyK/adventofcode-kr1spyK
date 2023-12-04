f68be2052d0f53765ab0f8865b52f1f614c0a045
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-03 20:51:47 -0500

    edit to output solution for both parts

--- day1.py
+++ day1.py
@@ -4,23 +4,23 @@
 # assumes calibration document is a reasonable size of arbitrary length lines with non-zero value.
 import re
 
-file = open('input1.txt', 'r')
-lines = file.readlines()
+with open('input1.txt', 'r') as fin:
+    lines = file.readlines()
 
-calibration_sum = 0
+solution1 = 0
+solution2 = 0
 
 num_pattern = re.compile('(?=(\d|one|two|three|four|five|six|seven|eight|nine))', re.IGNORECASE)
 num_words = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
 for line in lines:
 # Part one solution only looks for digits.
-#     digits = [int(x) for x in line if x.isdigit()]
-#     calibration_val = digits[0] * 10 + digits[-1]
-#     calibration_sum += calibration_val
+    digits = [int(x) for x in line if x.isdigit()]
+    calibration_val = digits[0] * 10 + digits[-1]
+    solution1 += calibration_val
 
 # Part two solution, looks for digits and written numerals (one, two, three, ..., seven, eight, nine).
-# reuses some part one code, assume input file is lowercase but checks anyway.
-# at first I thought about trying to find() all the word strings, then the digits and map them or something
+# logic is non case-sensitive, at first I thought about trying to find() all the word strings, then the digits and map them or something
 # too convoluted, and I found that regex can do it all with lookaround and zero-length assertions. sidenote: Stephen Kleene is an absolute madman
     digits = num_pattern.findall(line)
 
@@ -30,7 +30,6 @@ for line in lines:
     try: last = int(digits[-1])
     except: last = num_words[digits[-1]]
 
-    calibration_sum += first * 10 + last
+    soltuion2 += first * 10 + last
 
-print(calibration_sum)
-file.close()
+print(soltuion1, solution2)
