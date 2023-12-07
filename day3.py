45056641bb332e2f6c5577f4ad77726fdec451ba
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-07 15:39:14 -0500

    day 3 part 2 add

--- day3.py
+++ day3.py
@@ -6,41 +6,23 @@ Periods (.) are empty spaces and do not count as symbols.
 Return sum of all "part numbers".
 '''
 
-import re
+import re, math
 
-with open("test3.txt") as fin:
+with open("input3.txt") as fin:
     schematic = [line.strip() for line in fin]
 
 # Search for groups of numbers and save coords for each digit
-num_coords = []
-# sym_coords = {}
-adj_nums = []
-search_group = []
-adj = [(k, l) for k in (-1, 0, 1)
-              for l in (-1, 0, 1) if k != 0 or l != 0]
+sym_coords = {(r, c): [] for r in range(len(schematic)) for c in range(len(schematic[r]))
+                         if schematic[r][c] not in '123456789.'}
 
 for row, line in enumerate(schematic):
-    num_collector = re.finditer(r'\d+', line)
-    for num_match in num_collector:
-        digit_coords = []
+    for num_match in re.finditer(r'\d+', line):
         span = num_match.span()
+        adj = {(x, y) for x in (row - 1, row, row + 1) if x >= 0
+                      for y in range(span[0] - 1, span[1] + 1) if y >= 0}
+        
+        for hit in adj & sym_coords.keys():
+            sym_coords[hit].append(int(num_match.group()))
 
-        # start = span[0], end = span[1]
-        for i in range(span[1] - span[0]):
-            digit_coords.append((row, span[0] + i))
-        num_coords.append([num_match.group(), digit_coords])
-
-    sym_collector = re.finditer(r'[^.\d]', line)
-    for sym_match in sym_collector:
-        for pair in [(row + r, sym_match.start() + c) for r, c in adj]:
-            search_group.append(pair)
-        # sym_coords[(row, sym_match.start())] = sym_match.group()
-
-for group in num_coords:
-    for coord in group[1]:
-        if coord in search_group:
-            adj_nums.append(int(group[0]))
-            break
-
-# print('adj', adj_nums)
-print('sum', sum(adj_nums))
+print(sum(sum(s) for s in sym_coords.values()))
+print(sum(math.prod(s) for s in sym_coords.values() if len(s) == 2))
