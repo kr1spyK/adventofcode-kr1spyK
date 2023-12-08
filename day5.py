d0c2facd52aa82052174d2dec845366370908181
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-08 04:58:06 -0500

    Add day5 outline

---
+++ day5.py
@@ -0,0 +1,5 @@
+'''
+2023 Advent of Code - Day 05: If You Give A Seed A Fertilizer
+(https://adventofcode.com/2023/day/5)
+Almanac lists seeds to plant and mix with which soil, fertilizer, water.
+'''
