ff5dd3addd4c4ffbc1a30d56faf661d658b2418a
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-08 04:47:53 -0500

    Update day4 to part 2 compatibility

--- day4.py
+++ day4.py
@@ -8,14 +8,21 @@ Scratchcards made of lists of winning numbers and playing numbers, separated by 
 # First match = one point, subsequent matches double the total value.
 point_total = 0
 with open("input4.txt") as fin:
-    for card in fin:
-        card = card.strip().partition(':')
-        numbers = card[2].partition('|')
-        winning = {int(n) for n in numbers[0].split()}
-        players = {int(n) for n in numbers[2].split()}
+    cards = fin.readlines()
+    card_count = [1] * len(cards)
 
-        win = [n for n in winning if n in players]
-        points = 2**(len(win) - 1) if win else 0
+    for i, card in enumerate(cards):
+        numbers = card.strip().partition(':')[2]
+        winning, players = numbers.split('|')
+        winning, players = winning.split(), players.split()
 
-        point_total += points
-        # print(f"{card[0]}: {points} won, {point_total} total")
+        w = len(set(winning) & set(players))
+
+        point_total += 2**(w - 1) if w else 0
+
+        # print(f" {point_total} total")
+        # Part 2: cards reward cards
+        for j in range(w):
+            card_count[i + j + 1] += card_count[i]
+
+print(sum(card_count))
