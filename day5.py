904b689a4952115af906e3f8325b9af309e91d3e
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-09 15:36:06 -0500

    Add brute force solution day5 part 2

--- day5.py
+++ day5.py
@@ -10,9 +10,27 @@ def grouper(iterable, n, *, incomplete='ignore', fillvalue=None):
     args = [iter(iterable)] * n
     return zip(*args)
     
-def splitcategory(n):
+def splitcategory(n: str):
+    "Split colon separated string 'name:ints' into (name, [ints])"
     n = n.partition(':')
     return (n[0].strip(), list(map(int, n[2].split())))
+
+def seedtoleastlocation(seeds: list, almanac: dict, location=9999999999):
+    "Return lowest location of given seed list or location"
+    # Assumes almanac maps are ordered from seed -> ... -> location
+    location = 9999999999 if location == None else location
+    if not almanac or len(seeds) == 0:
+        return 0
+    
+    for s in seeds:
+        for cat in almanac.values():
+            for plot in cat:
+                dst, src, stp = plot
+                if s in range(src, src + stp):
+                    s = s-src+dst
+                    break
+        location = min(s, location)
+    return location
 
 with open("input5.txt") as fin:
     content = fin.read().split('\n')
@@ -28,18 +46,15 @@ seeds = maps.pop(0)[1]
 almanac = {cat : list(grouper(nums,3)) for cat, nums in maps}
 
 # Part 1 solution - Dynamic solution by getting seeds index if its mapped.
-# Assumes almanac maps are ordered from seed -> ... -> location
-location = []
-for s in seeds:
-    # print("current seed:", s)
-    for cat in almanac.values():
-        for plot in cat:
-            dst, src, stp = plot
-            if s in range(src, src + stp):
-                # print(s, end=' -> ')
-                s = s-src+dst
-                break
-        # print(s)
-    location.append(s)
+print('part1:', seedtoleastlocation(seeds, almanac))
 
-print('lowest:', min(location))
+# Part 2 solution
+# Seeds are actually ranges instead of locations, taken by pairs (start, length)
+seeds = grouper(seeds, 2)
+
+# Top-down brute force still takes forever for large inputs
+# sneed: int = None
+# for s, l in seeds:
+#     for n in range(s, s+l):
+#         sneed = leastlocation([n], almanac, sneed)
+# print('part2:', sneed)
