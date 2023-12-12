6dc97084c2aedb83da684b05a6ea7e2f86dda957
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-12 01:22:53 -0500

    Add day5 part 2 brute force tabulation

--- day5.py
+++ day5.py
@@ -15,22 +15,32 @@ def splitcategory(n: str):
     n = n.partition(':')
     return (n[0].strip(), list(map(int, n[2].split())))
 
-def seedtoleastlocation(seeds: list, almanac: dict, location=9999999999):
-    "Return lowest location of given seed list or location"
+def trace_almanac(input, output=9999999999):
+    "Input list of seeds to get smallest almanac location, takes single ints as a location to backtrack to seed"
     # Assumes almanac maps are ordered from seed -> ... -> location
-    location = 9999999999 if location == None else location
-    if not almanac or len(seeds) == 0:
+    output = 9999999999 if output == None else output
+    if input == None:
         return 0
     
-    for s in seeds:
-        for cat in almanac.values():
-            for plot in cat:
-                dst, src, stp = plot
-                if s in range(src, src + stp):
-                    s = s-src+dst
+    if output > 0:
+        input = [input] if type(input) != list else input
+        for s in input:
+            for cat in almanac.values():
+                for dst, src, stp in cat:
+                    if s in range(src, src + stp):
+                        s = s-src+dst
+                        break
+            output = min(s, output)
+
+    if output < 0:
+        output = int(input)
+        for cat in reversed(almanac.values()):
+            for ds, sr, z in cat:
+                if output in range(ds, ds + z):
+                    output = output-ds+sr
                     break
-        location = min(s, location)
-    return location
+
+    return output
 
 with open("input5.txt") as fin:
     content = fin.read().split('\n')
@@ -46,15 +56,32 @@ seeds = maps.pop(0)[1]
 almanac = {cat : list(grouper(nums,3)) for cat, nums in maps}
 
 # Part 1 solution - Dynamic solution by getting seeds index if its mapped.
-print('part1:', seedtoleastlocation(seeds, almanac))
+print('part1:', trace_almanac(seeds))
 
 # Part 2 solution
 # Seeds are actually ranges instead of locations, taken by pairs (start, length)
-seeds = grouper(seeds, 2)
-
 # Top-down brute force still takes forever for large inputs
-# sneed: int = None
-# for s, l in seeds:
-#     for n in range(s, s+l):
-#         sneed = leastlocation([n], almanac, sneed)
-# print('part2:', sneed)
+
+seeds = [(s, s+w-1) for s, w in grouper(seeds, 2)]
+# max_location = 0
+# for d, s, p in next(reversed(almanac.values())):
+#     max_location = max(d, max_location)
+smol = (99999999999,99999999999999)
+low_loc = 99999999999999999999
+seed_map = set()
+for group in seeds:
+    a = tuple(map(trace_almanac, group))
+    seed_map.add(range(min(group), max(group) + 1))
+    if low_loc > min(a):
+        smol = group
+        low_loc = min(a)
+
+found = None
+n = 0
+while n < low_loc + 1 and not found:
+    n += 1
+    s = trace_almanac(n, -1)
+    for s_range in seed_map:
+        if s in s_range:
+            found = s
+print('part2:', n)
