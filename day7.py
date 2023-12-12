61e2968cf9c85761d368d35bcdfda17cb494f5d2
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-12 03:13:43 -0500

    Outline day7

---
+++ day7.py
@@ -0,0 +1,6 @@
+'''
+2023 Advent of Code - Day 06: Wait For IT
+(https://adventofcode.com/2023/day/6)
+A "water rally" time card gives max time allowed and farthest distance for a course.
+Toy boats are charged up by holding a button and move once released. Charge starts the clock.
+'''
