cf903e27c93dd1e0913c72697f2c5c8741b1d5a9
Krispy <42679658+kr1spyK@users.noreply.github.com>
2023-12-12 03:11:27 -0500

    Compete day6

--- day6.py
+++ day6.py
@@ -4,6 +4,15 @@
 A "water rally" time card gives max time allowed and farthest distance for a course.
 Toy boats are charged up by holding a button and move once released. Charge starts the clock.
 '''
+def td_calc(td: list):
+    permute = 1
+    for t, d in td:
+        wins = 0
+        for i in range(1, t):
+            if i*(t-i) > d:
+                wins += 1
+        permute *= wins
+    return permute
 
 with open("input6.txt") as fin:
     time = fin.readline().partition(':')[2].split()
@@ -13,13 +22,10 @@ with open("input6.txt") as fin:
 # Boat starts at zero speed, each ms of charging gives 1 mm/ms
 # Return total permutations where you beat records in each race.
 td = list(zip([int(i) for i in time], [int(i) for i in dist]))
-permute = 1
-for t, d in td:
-    wins = 0
-    for i in range(1, t):
-        race = i*(t-i)
-        if race > d:
-            wins += 1
-    # print('wins', wins)
-    permute *= wins
-print(permute)
+part1 = td_calc(td)
+print('part1:', part1)
+
+# Part 2: input is actually records for one race, kerning is off.
+# EZIST PART YET omg
+td= (int(''.join(time)), int(''.join(dist)))
+print(td_calc([td]))
